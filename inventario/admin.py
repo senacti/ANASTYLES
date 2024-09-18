816069037bc0e66056b3a2e8c0d06d6e2f04dc54
Oscar <oscarmontoya119@gmail.com>
2024-09-18 08:12:50 -0500

    se agrega inventario

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Productos, Ubicacion_inv,Inventario
+
+admin.site.register(Productos)
+admin.site.register(Inventario)
+admin.site.register(Ubicacion_inv)
 
 # Register your models here.
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from produccion.models import Produccion
 
+class Productos(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_product = models.ForeignKey(Produccion, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.descripcion
+    
+class Ubicacion_inv(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Inventario(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_producto = models.ForeignKey(Productos, on_delete=models.CASCADE)
+    id_ubicacion = models.ForeignKey(Ubicacion_inv, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcion
 # Create your models here.
+
+
+
+ 
