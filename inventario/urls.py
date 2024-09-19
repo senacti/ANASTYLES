49e34ef53b39ed7aba1be914eea592c6eaa9b801
Oscar <oscarmontoya119@gmail.com>
2024-09-19 10:50:16 -0500

    se intenta agregar inventarios ( no mmover plz)

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ProductosListView
 
 urlpatterns = [
-
+    path('productos/', ProductosListView.as_view(), name='productos_list'),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -3,9 +3,62 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Productos, Inventario, Ubicacion_inv
 
-class ProductosListViews(ListView):
+class ProductosListView(ListView):
     model = Productos
-    template_name = 'inventario/productos_lista.html'
+    template_name = 'inventario/productos_list.html'
+    context_object_name = 'productos'
 
+class ProductosDetailView(DetailView):
+    models = Productos
+    template_name = 'inventario/productos_detail.html'
+
+class ProductosCreateView(CreateView):
+    model = Productos
+    fields = ['id_product', 'descripcion', 'precio', 'cantidad']
+    template_name = 'inventario/productos_form.html'
+    success_url = reverse_lazy('productos_list')
+
+class ProductosCreateView(CreateView):
+    model = Productos
+    fields = ['id_product', 'descripcion', 'precio', 'cantidad']
+    template_name = 'inventario/productos_form.html'
+    success_url = reverse_lazy('productos_list')
+
+class ProductosUpdateView(UpdateView):
+    model = Productos
+    fields = ['id_product', 'descripcion', 'precio', 'cantidad']
+    template_name = 'inventario/productos_form.html'
+    success_url = reverse_lazy('productos_list')
+
+class ProductosDeleteView(DeleteView):
+    model = Productos
+    template_name = 'inventario/productos_confirm_delete.html'
+    success_url = reverse_lazy('productos_list')
+
+# Inventario CRUD
+class InventarioListView(ListView):
+    model = Inventario
+    template_name = 'inventario/inventario_list.html'
+
+class InventarioDetailView(DetailView):
+    model = Inventario
+    template_name = 'inventario/inventario_detail.html'
+
+class InventarioCreateView(CreateView):
+    model = Inventario
+    fields = ['id_producto', 'id_ubicacion', 'descripcion']
+    template_name = 'inventario/inventario_form.html'
+    success_url = reverse_lazy('inventario_list')
+
+class InventarioUpdateView(UpdateView):
+    model = Inventario
+    fields = ['id_producto', 'id_ubicacion', 'descripcion']
+    template_name = 'inventario/inventario_form.html'
+    success_url = reverse_lazy('inventario_list')
+
+class InventarioDeleteView(DeleteView):
+    model = Inventario
+    template_name = 'inventario/inventario_confirm_delete.html'
+    success_url = reverse_lazy('inventario_list')
 
 # Create your views here.
